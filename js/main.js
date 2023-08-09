f8eda6cdb1f6d1d57fdb7a277e14debacb5d6f2e
John Idoko <idokojohn42@yahoo.com>
2023-08-09 16:59:03 +0100

    QOS checkout

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const tabButtons = document.querySelectorAll('.tab-button');
+const tabPanes = document.querySelectorAll('.tab-pane');
+
+function changeTab(index) {
+    tabButtons.forEach((button, i) => {
+        button.classList.remove('active');
+        tabPanes[i].classList.remove('active');
+    });
+
+    tabButtons[index].classList.add('active');
+    tabPanes[index].classList.add('active');
+}
+
+// Initially, activate the first tab
+changeTab(1);
